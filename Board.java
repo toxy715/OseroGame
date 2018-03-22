a532b5812451d1438258852a5d422109f24624a8
Toxy715 <sarimerienjel15@yahoo.co.jp>
2018-03-22 18:14:14 +0900

    2 commit

--- Board.java
+++ Board.java
@@ -1,14 +1,16 @@
 import java.awt.*;
-
+// 定数
 public class Board {
     // フィールド
     private String[][] board;
+    public static final int CELL = 8;
+    public static final int CELL_CHECK = 6;
     
     // コンストラクタ
     public Board() {
-	this.board = new String[8][];
-	for (int i = 0; i < 8; i++) {
-	    this.board[i] = new String[8];
+	this.board = new String[6][];
+	for (int i = 0; i < 6; i++) {
+	    this.board[i] = new String[6];
 	}
     }
 
@@ -23,27 +25,57 @@ public class Board {
 
     public void print() {
 	// 盤面の表示
-	for (int y = 0; y < 8; y++) {
-	    for (int x = 0; x < 8; x++) {
+	int i = 0;
+	int j = 0;
+	System.out.print("  " + i);
+	for(i = 1; i < CELL; i++){
+	    System.out.print(" " + i);
+	}
+	System.out.print(" (X)");
+	System.out.println();
+	for (int y = 0; y < CELL; y++) {
+	    System.out.print(" " + j);
+	    j++;
+	    for (int x = 0; x < CELL; x++) {
 		System.out.print(board[x][y]);
 	    }
 	    System.out.println();
 	}
-	System.out.println();
+	System.out.println("(Y)");
     }
-
+    
     //確認用
-    /*
+    public void print_check() {
+	// 盤面の表示
+	int i = 0;
+	int j = 0; 
+	System.out.print(i);
+	for(i = 1; i < CELL_CHECK; i++){
+	    System.out.print(" " + i);
+	}
+	System.out.print(" (X)");
+	System.out.println();
+	for(int y = 0; y < CELL_CHECK; y++) {
+	    for(int x = 0; x < CELL_CHECK; x++) {
+		System.out.print(board[x][y]);
+	    }
+	    System.out.print(" " + j);
+	    j++;
+	    System.out.println();
+	}
+	System.out.println("　　　　　　　　(Y)");
+    }
+    
     public static void main(String[] args) {
 	Board b = new Board();
-	for (int y = 0; y < 8; y++) {
-	    for (int x = 0; x < 8; x++) {
+	for (int y = 0; y < CELL_CHECK; y++) {
+	    for (int x = 0; x < CELL_CHECK; x++) {
 		String koma = "□";
 		//int i = 0;
 		b.setCell(x, y, koma);
 	    }
 	}
-	b.print();
+	b.print_check();
     }
-    */
+    
 }
